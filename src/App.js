c899dc50e48c1e30c630dcdfd5f81f3fc5441d33
John Wang <johnjinghuawang@gmail.com>
2020-10-17 12:25:35 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
+import { Steps, Divider } from 'antd';
+const { Step } = Steps;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
+      <>
+        <Steps progressDot current={1}>
+          <Step title="Finished" description="This is a description." />
+          <Step title="In Progress" description="This is a description." />
+          <Step title="Waiting" description="This is a description." />
+        </Steps>
+        <Divider />
+        React and Ant Design are work! Wow!
+      </>,
     </div>
   );
 }
