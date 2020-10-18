fc95bec1ccfa953760d9e35c5a2e04ef47b7e24c
fmn0012300 <fmn4@uw.edu>
2020-10-17 23:19:09 -0700

    Basic implementation on homepage

--- src/Trips.js
+++ src/Trips.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck'
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/cjs/Tooltip";
 
 import './Trips.css';
 /**
@@ -15,12 +17,17 @@ class Trips extends Component{
         for (let i = 0; i < this.props.card.length; i++) {
             let card = this.props.card[i];
             result.push(
-                (   <Card className={"cards"} onClick={()=>{window.location.assign(card["tripStartUrl"])}}>
-                        <Card.Img variant="top" src={"img" + card["img_url"]}/>
-                        <Card.Body>
-                            {card["name"]}
-                        </Card.Body>
-                    </Card>
+                (
+                    <OverlayTrigger
+                        placement="bottom"
+                        overlay={<Tooltip id="button-tooltip-2">{card["name"]}</Tooltip>}
+                    >
+                        <Card style={{backgroundImage: "url(img" + card["img_url"] + ")",
+                                        backgroundSize: "cover", display: "flex" }}
+                              className={"cards"}
+                              onClick={()=>{window.location.assign(card["tripStartUrl"])}}>
+                        </Card>
+                    </OverlayTrigger>
                 )
             );
         }
